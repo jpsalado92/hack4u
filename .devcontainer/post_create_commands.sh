1b0fdcf28e6615c33e70563e6473f668d1a83847
jpsalado <juan.salado@optimeeringaqua.com>
2023-11-16 17:31:45 +0000

    chchchangesss

---
+++ .devcontainer/post_create_commands.sh
@@ -0,0 +1,6 @@
+# apt-get update
+# apt install zsh -y
+# yes Y | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) -y" 
+# clear
+# zsh
+# compinit
---
+++ customization.md
@@ -0,0 +1,8 @@
+
+In order to include customizations we use the file `.bashrc` or `zshrc`.
+
+Used for:
+
+Create functions.
+Create utilities.
+
