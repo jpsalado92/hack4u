01a81e7f494339d6b70e9a6cc2646e14c69779f4
JPS <jpsalado92@gmail.com>
2023-11-20 17:23:01 +0000

    Changes

--- .devcontainer/post_create_commands.sh
+++ .devcontainer/post_create_commands.sh
@@ -6,19 +6,19 @@ apt-get update
 # Get tmux
 apt install tmux -y
 
-# Get ohmytmux
-cd
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
+# # Get ohmytmux
+# cd
+# git clone https://github.com/gpakosz/.tmux.git
+# ln -s -f .tmux/.tmux.conf
+# cp .tmux/.tmux.conf.local .
 
 
-# Get zsh
-apt install zsh -y
+# # Get zsh
+# apt install zsh -y
 
-# Get ohmyzsh
-yes Y | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) -y" 
-clear
+# # Get ohmyzsh
+# yes Y | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) -y" 
+# clear
 
-# Set zsh as default shell
-chsh -s /bin/zsh 
+# # Set zsh as default shell
+# chsh -s /bin/zsh 
