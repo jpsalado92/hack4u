c40083885bcd1d0c221b3f5592da98d1737a2e3c
jpsalado <juan.salado@optimeeringaqua.com>
2024-03-02 12:03:22 +0100

    wip

--- .devcontainer/victim-ubuntu/post_start_commands.sh
+++ .devcontainer/victim-ubuntu/post_start_commands.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Get information about the current network
-LAN_IP=$(/workspace/src/attacker-archlinux/tools/bash-grab-local-ip/grabLocalIP.sh)
+LAN_IP=$(hostname -I)
 echo "Your local IP address is: $LAN_IP"
-NETWORK_IPS=$(/workspace/src/attacker-archlinux/tools/bash-grab-local-ip-ranges/grabLocalIPRanges.sh)
-echo "Your network local IPs are: $NETWORK_IPS"
+echo ""
+cat /etc/lsb-release
